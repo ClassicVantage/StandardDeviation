d07ee458f592fd5d95d0437ae7f068b72e269a69
ClassicVantage <61285209+ClassicVantage@users.noreply.github.com>
2021-05-22 11:17:23 +0530

    ok

---
+++ StandardDeviationProject.py
@@ -0,0 +1,29 @@
+import csv 
+
+with open("data,csv",newline='') as f:
+    reder=csv.reader(f)
+    file_data=list(reader)
+
+data=file_data[0]
+
+#finding mean
+def mean(data):
+    n=len(data)
+    total=0
+    for x in data:
+        total+=int(x)
+    mean=total/n 
+    return mean
+
+sqaredList=[]
+for no in data:
+    a=int (no)-mean(data)
+    a=a**2
+    squaredList.append(a)
+
+sum=0
+for i in squaredList:
+    sum=sum+i
+result=sum/(len(data)-1)
+sd=math.sqrt(result)
+print(sd)
